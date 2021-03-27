021a31d96ae2fd8b21e6295414bd12c8b2e6c8a9
Lee Jeanty <leejeanty1@yahoo.com>
2021-03-26 23:43:43 -0400

    add a file path to this module

---
+++ filepath.py
@@ -0,0 +1,3 @@
+cameraPic = "/home/pi/picam/pictures"
+cameraVid = "/home/pi/picam/videos"
+cameraLog = "/home/pi/picam/logs"
--- recordVideo.py
+++ recordVideo.py
@@ -1,9 +1,10 @@
 import picamera
+import filepath
 
 
 def startRec():
     camera = picamera.PiCamera()
     camera.resolution = (1280, 720)
-    camera.start_recording('test.h264')
+    camera.start_recording(filepath.cameraVid)
     camera.wait_recording(10)
     camera.stop_recording()
