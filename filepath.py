4d82975430ebe8a3cd2434a35700ede8ddfc6dc8
Lee Jeanty <leejeanty1@yahoo.com>
2021-03-26 23:54:51 -0400

    fixed error add correct filepath name

--- filepath.py
+++ filepath.py
@@ -1,3 +1,16 @@
-cameraPic = "/home/pi/picam/pictures"
-cameraVid = "/home/pi/picam/videos"
-cameraLog = "/home/pi/picam/logs"
+
+camera_Pic = "/home/pi/picam/pictures"
+camera_Vid = "/home/pi/picam/videos"
+camera_Log = "/home/pi/picam/logs"
+
+
+def cameraPic(timeStamp):
+    return camera_Pic + "{timeStamp}" + ".h264"
+
+
+def cameraVid(timeStamp):
+    return camera_Vid + "{timeStamp}" + ".h264"
+
+
+def cameraLog(timeStamp):
+    return camera_Log + "{timeStamp}" + ".h264"
--- recordVideo.py
+++ recordVideo.py
@@ -1,10 +1,14 @@
 import picamera
 import filepath
+import time
+
+timeStamp = time.localtime()
+print(timeStamp)
 
 
 def startRec():
     camera = picamera.PiCamera()
     camera.resolution = (1280, 720)
-    camera.start_recording(filepath.cameraVid)
+    camera.start_recording(filepath.cameraVid(timeStamp))
     camera.wait_recording(10)
     camera.stop_recording()
