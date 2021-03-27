66dcab3938265c2c1ee3a543c1d10e7e70fab18d
Lee Jeanty <leejeanty1@yahoo.com>
2021-03-26 22:01:48 -0400

    test

---
+++ recordVideo.py
@@ -0,0 +1,8 @@
+import picamera
+
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.start_recording('test.h264')
+    camera.wait_recording(10)
+    camera.stop_recording()
