0fbcde92bbdef6ab7b56bdbd6b4b72ced19aa049
Lee Jeanty <leejeanty1@yahoo.com>
2021-03-29 01:15:48 -0400

    trying something out

--- recordVideo.py
+++ recordVideo.py
@@ -10,8 +10,8 @@ print(timeStamp)
 
 
 def startRec():
-    camera = picamera.PiCamera()
-    camera.resolution = (1280, 720)
-    camera.start_recording(filepath.cameraVid(timeStamp))
-    camera.wait_recording(10)
-    camera.stop_recording()
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        camera.start_recording(filepath.cameraVid(timeStamp))
+        camera.wait_recording(10)
+        camera.stop_recording()
