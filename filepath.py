e8d0645e8345bfd7a0180ff976b980a3faa4bb18
Lee Jeanty <leejeanty1@yahoo.com>
2021-03-28 20:54:55 -0400

    Fixed comma

--- filepath.py
+++ filepath.py
@@ -13,7 +13,7 @@ def cameraPic(timeStamp):
             os.makedirs(camera_Pic, True)
             return camera_Pic + timeStamp + ".h264"
     except OSError as error:
-        print("Directory '%s' can not be created")
+        print("Directory can not be created")
     return camera_Pic + timeStamp + ".h264"
 
 
@@ -25,17 +25,17 @@ def cameraVid(timeStamp):
             os.makedirs(camera_Vid, True)
             return camera_Vid + timeStamp + ".h264"
     except OSError as error:
-        print("Directory '%s' can not be created")
+        print("Directory can not be created")
     return camera_Vid + timeStamp + ".h264"
 
 
 def cameraLog(timeStamp):
     try:
-        if os.makedirs(camera_Log, True), == True:
+        if os.makedirs(camera_Log, True) == True:
             print("Path " + camera_Log + " Already exist")
         else:
             os.makedirs(camera_Log, True)
             return camera_Log + timeStamp + ".h264"
     except OSError as error:
-        print("Directory '%s' can not be created")
+        print("Directory can not be created")
     return camera_Log + timeStamp + ".h264"
