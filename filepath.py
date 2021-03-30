8434a324df3abcdbb613235e7b42e41d25b38051
Lee Jeanty <leejeanty1@yahoo.com>
2021-03-29 22:57:00 -0400

    Loggin

--- filepath.py
+++ filepath.py
@@ -7,22 +7,26 @@ camera_Log = "/home/pi/picam/logs/"
 
 def cameraPic(timeStamp):
     print(str(os.path.isdir(camera_Pic)))
+    print("Gonna take a Pic")
     if os.path.isdir(camera_Pic) == True:
         print("Path: " + camera_Pic + " Already exist")
         return camera_Pic + timeStamp + ".jpg"
     else:
         os.makedirs(camera_Pic)
         return camera_Pic + timeStamp + ".jpg"
+    print("Took a Pic")
 
 
 def cameraVid(timeStamp):
     print(str(os.path.isdir(camera_Vid)))
+    print("Started Recording")
     if os.path.isdir(camera_Vid) == True:
         print("Path: " + camera_Vid + " Already exist")
         return camera_Vid + timeStamp + ".h264"
     else:
         os.makedirs(camera_Vid)
         return camera_Vid + timeStamp + ".h264"
+    print("Done Recording")
 
 
 def cameraLog(timeStamp):
