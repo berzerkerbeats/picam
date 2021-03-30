8be53a5af18e292809e367a8bf8b0c152201cf27
Lee Jeanty <leejeanty1@yahoo.com>
2021-03-29 22:57:00 -0400

    Loggin

--- takePic.py
+++ takePic.py
@@ -8,13 +8,8 @@ timeStamp = now.strftime("%m_%d_%Y_%H:%M:%S")
 
 print(timeStamp)
 
-print("Gonna take a Pic")
-
 
 def startPic():
     with picamera.PiCamera() as camera:
         camera.resolution = (1280, 720)
         camera.capture(filepath.cameraPic(timeStamp))
-
-
-print("Took a Pic")
