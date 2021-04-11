aa1dfa706135b4a314b07b626a2fa90e9f2a81ab
Lee Jeanty <leejeanty1@yahoo.com>
2021-04-11 12:36:19 -0400

    Added some logging to main

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import recordVideo
 import takePic
 import time
+import logging
+import filepath
 
 
 def main():
     recordVideo.startRec()
+    filepath.cameraLog(logging.info(" Done Recording"))
+    logging.info("Done Recording")
     time.sleep(5)
     takePic.startPic()
+    filepath.cameraLog(logging.info(" Done with photo"))
+    logging.info(" Done with photo")
 
 
 if __name__ == '__main__':
