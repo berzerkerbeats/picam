ef9ef002bf72ad94633e4edc642bf6243e9e0814
Lee Jeanty <leejeanty1@yahoo.com>
2021-04-14 10:19:57 -0400

    Update

--- filepath.py
+++ filepath.py
@@ -33,8 +33,6 @@ def cameraVid(timeStamp):
 
     print(cameraLog(str(logging.info(" Started Recording"))))
 
-    logging.info("Started Recording")
-
     if os.path.isdir(camera_Vid) == True:
         print("Path: " + camera_Vid + " Already exist")
         return camera_Vid + timeStamp + ".h264"
@@ -57,8 +55,8 @@ def cameraLog(logs):
             return camera_Log + toLogs.timeStamp + ".log"
 
     except OSError:
-        print(str(logging.critical("Could not make dir " +
-              camera_Log + " check systems!!!")))
+        print(cameraLog(str(logging.critical("Could not make dir " +
+              camera_Log + " check systems!!!"))))
 
     try:
         print("Path: " + camera_Log + " Already exist")
