0d93338edee14491dc3ff4e659dfa0b335087125
Lee Jeanty <leejeanty1@yahoo.com>
2021-04-14 21:30:35 -0400

    Update Logging

--- filepath.py
+++ filepath.py
@@ -10,18 +10,18 @@ camera_Log = "/home/pi/picam/logs/"
 def cameraPic(timeStamp):
 
     print(str(os.path.isdir(camera_Pic)))
-    print(cameraLog(str(logging.info(" Taking a picture "))))
+    print(str(logging.info(" Taking a picture ")))
 
     if os.path.isdir(camera_Pic) == True:
         print()
-        print(cameraLog(str(logging.info(" Path: " + camera_Pic + " Already exist"))))
+        print(str(logging.info(" Path: " + camera_Pic + " Already exist")))
         return camera_Pic + timeStamp + ".jpg"
 
     else:
         os.makedirs(camera_Pic)
         return camera_Pic + timeStamp + ".jpg"
 
-    print(cameraLog(str(logging.info(" Took a picture"))))
+    print(str(logging.info(" Took a picture")))
 
 
 def cameraVid(timeStamp):
@@ -29,7 +29,7 @@ def cameraVid(timeStamp):
     print(str(os.path.isdir(camera_Vid)))
     print(str(logging.info(os.path.isdir(camera_Vid))))
 
-    cameraLog(logging.info(os.path.isdir(camera_Vid)))
+    print(logging.info(os.path.isdir(camera_Vid)))
 
     print(logging.info(" Started Recording"))
 
@@ -41,13 +41,13 @@ def cameraVid(timeStamp):
         os.makedirs(camera_Vid)
         return camera_Vid + timeStamp + ".h264"
 
-    print(cameraLog(logging.info("Done Recording")))
+    print(logging.info("Done Recording"))
 
 
 def cameraLog(logs):
     try:
         if os.path.isdir(camera_Log) == False:
-            print(cameraLog(logging.info("Making Dir: " + camera_Log + " !")))
+            print(logging.info("Making Dir: " + camera_Log + " !"))
             os.makedirs(camera_Log)
 
         elif os.path.isdir(camera_Log) == True:
@@ -55,8 +55,8 @@ def cameraLog(logs):
             return camera_Log + toLogs.timeStamp + ".log"
 
     except OSError:
-        print(cameraLog(logging.critical("Could not make dir " +
-              camera_Log + " check systems!!!")))
+        print(logging.critical("Could not make dir " +
+              camera_Log + " check systems!!!"))
 
     try:
         print("Path: " + camera_Log + " Already exist")
