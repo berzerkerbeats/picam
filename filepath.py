abd0c2df2735d4c85f96e3d7ac621fa45912d2e0
Lee Jeanty <leejeanty1@yahoo.com>
2021-04-17 03:07:44 -0400

    logging the cl output

--- filepath.py
+++ filepath.py
@@ -11,61 +11,61 @@ def cameraPic(timeStamp):
 
     print(str(os.path.isdir(camera_Pic)))
 
-    cameraLog(logging.info(" Taking a picture "))
+    logging.info(" Taking a picture ")
 
     if os.path.isdir(camera_Pic) == True:
-        print(cameraLog(logging.info(
-            " Path: " + camera_Pic + " Already exist")))
+        logging.info(
+            " Path: " + camera_Pic + " Already exist")
         return camera_Pic + timeStamp + ".jpg"
 
     else:
         os.makedirs(camera_Pic)
         return camera_Pic + timeStamp + ".jpg"
 
-    print(cameraLog(logging.info(" Took a picture")))
+    logging.info(" Took a picture")
 
 
 def cameraVid(timeStamp):
 
-    cameraLog(os.path.isdir(camera_Vid))
+    logging.info(os.path.isdir(camera_Vid))
 
-    cameraLog(logging.info(os.path.isdir(camera_Vid)))
+    logging.info(os.path.isdir(camera_Vid))
 
-    cameraLog(logging.info(os.path.isdir(camera_Vid)))
+    logging.info(os.path.isdir(camera_Vid))
 
-    cameraLog(logging.info(" Started Recording"))
+    logging.info(" Started Recording")
 
     if os.path.isdir(camera_Vid) == True:
-        cameraLog(logging.info(
-            " Path: " + camera_Vid + " Already exist"))
+        logging.info(
+            " Path: " + camera_Vid + " Already exist")
         return camera_Vid + timeStamp + ".h264"
 
     else:
         os.makedirs(camera_Vid)
         return camera_Vid + timeStamp + ".h264"
 
-    cameraLog(logging.info("Done Recording"))
+    logging.info("Done Recording")
 
 
-def cameraLog(logs):
-    try:
-        if os.path.isdir(camera_Log) == False:
-            cameraLog(logging.info(
-                "Making Dir: " + camera_Log + " !"))
-            os.makedirs(camera_Log)
+# def cameraLog(logs):
+#     try:
+#         if os.path.isdir(camera_Log) == False:
+#             cameraLog(logging.info(
+#                 "Making Dir: " + camera_Log + " !"))
+#             os.makedirs(camera_Log + toLogs.timestamp)
 
-        elif os.path.isdir(camera_Log) == True:
-            print(toLogs.writeToLog(str(logs)))
-            return camera_Log + toLogs.timeStamp + ".log"
+#         elif os.path.isdir(camera_Log) == True:
+#             toLogs.writeToLog(str(logs))
+#             return camera_Log + toLogs.timeStamp + ".log"
 
-    except OSError:
-        cameraLog(logging.critical("Could not make dir " +
-                                   camera_Log + " check systems!!!"))
+#     except OSError:
+#         cameraLog(logging.critical("Could not make dir " +
+#                                    camera_Log + " check systems!!!"))
 
-    try:
-        cameraLog(logging.info("Path: " + camera_Log + " Already exist"))
-        print(toLogs.writeToLog(str(logs)))
-        return camera_Log + toLogs.timeStamp + ".log"
+#     try:
+#         cameraLog(logging.info("Path: " + camera_Log + " Already exist"))
+#         toLogs.writeToLog(str(logs))
+#         return camera_Log + toLogs.timeStamp + ".log"
 
-    except OSError:
-        pass
+#     except OSError:
+#         pass
