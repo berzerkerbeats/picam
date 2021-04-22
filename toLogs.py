22a235eaccaaa081de8129be5d885946025da6d1
Lee Jeanty <leejeanty1@yahoo.com>
2021-04-21 21:27:11 -0400

    Added some updates

--- toLogs.py
+++ toLogs.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import filepath
 from datetime import datetime
@@ -5,6 +6,13 @@ from datetime import datetime
 now = datetime.now()
 
 timeStamp = now.strftime("%m_%d_%Y_%H:%M:%S")
+
+if os.path.isdir(filepath.camera_Log) == False:
+    logging.info("Making Dir: " + filepath.camera_Log + " !")
+    print("Making Dir: " + filepath.camera_Log + " !")
+    logging.info("Made Dir !")
+    print("Made Dir !")
+    os.makedirs(filepath.camera_Log + timeStamp)
 
 logging.basicConfig(filename="/home/pi/picam/logs/" + timeStamp, format='%(asctime)s:%(message)s',  level=logging.DEBUG,
                     datefmt='%m/%d/%Y %I:%M:%S %p')
