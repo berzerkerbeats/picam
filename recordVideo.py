a5caa2154136beed69e0e5b2e9bf222e80fc80cd
Lee Jeanty <leejeanty1@yahoo.com>
2021-06-19 04:56:35 -0400

    Added a way to keep watch

--- recordVideo.py
+++ recordVideo.py
@@ -6,11 +6,14 @@ now = datetime.now()
 
 timeStamp = now.strftime("%m_%d_%Y_%H:%M:%S")
 
+sec = 10
 
-def startRec():
+path = filepath.cameraVid(timeStamp)
+
+def startRec(self, sec, path):
     print(timeStamp)
     with picamera.PiCamera() as camera:
         camera.resolution = (1280, 720)
-        camera.start_recording(filepath.cameraVid(timeStamp))
-        camera.wait_recording(10)
+        camera.start_recording(path)
+        camera.wait_recording(sec)
         camera.stop_recording()
